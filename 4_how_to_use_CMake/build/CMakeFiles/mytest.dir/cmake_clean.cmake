7e7855d62609b00bb906fec7521ffa421e905730
alex-lee <leemh0512@naver.com>
2024-01-28 18:22:46 +0900

    Basic usage of CMake

---
+++ 4_how_to_use_CMake/build/CMakeFiles/mytest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytest.dir/main.c.o"
+  "CMakeFiles/mytest.dir/my.c.o"
+  "mytest"
+  "mytest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4_how_to_use_CMake/my.c
@@ -0,0 +1,7 @@
+/////////implementation of my_func. Output is the square of the input value.////////////////////////
+
+#include "my.h"
+
+int my_func(int x){
+	return x * x;
+}

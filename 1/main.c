dc05ec1b36399a6f85eb7f916aed31da7493a2a9
alex-lee <leemh0512@naver.com>
2024-01-28 15:18:41 +0900

    Learned how to include header files

---
+++ 1/main.c
@@ -0,0 +1,9 @@
+/////This is first practice src for Linux c development envlironment/////
+////made with Ubuntu20.04,gcc compiler/////
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!!!\n");
+
+	return 0;
+}
---
+++ 2/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "my.h"
+
+int main(){
+	printf("calling up mayfunc : %d\n", myfunc(3));
+	return 0;
+}
